c0c65f32fb71e4560da717e8efb027888e84b3d6
alinaqvii <wajahat.naqvi21@gmail.com>
2019-01-01 23:40:22 +0500

    Budget App 4

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -60,6 +60,21 @@ var budgetController = (function(){
             return newItem;
         },
 
+        deleteItem: function(type, id){
+            var ids, index
+            
+            ids = data.allItems[type].map(function(current){
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1){
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         calculateBudget: function(){
             // calculate the total income and expenses
                 calculateTotal('inc');
@@ -218,7 +233,26 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlDeleteItem = function(event){
-        
+        var itemID, splitID;
+
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if(itemID){
+
+            // inc-1
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = parseInt(splitID[1]);
+
+            // 1- delete the item from the data structure
+
+            budgetCtrl.deleteItem(type, ID);
+
+            // 2- Delete the item from UI
+
+            // 3- update and show the new budget
+
+        }
     };
 
     return{
