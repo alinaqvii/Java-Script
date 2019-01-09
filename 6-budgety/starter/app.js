5e8992888b6a43fb8b02254b50421d1de7d3e012
alinaqvii <wajahat.naqvi21@gmail.com>
2019-01-09 23:09:25 +0500

    Budget App 8-Current Date & Month

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -150,7 +150,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage' 
+        expensesPercLabel: '.item__percentage',
+        dateLabel:'.budget__title--month' 
     };
 
 
@@ -261,7 +262,21 @@ var UIController = (function() {
 
         },
         
+        displayMonth: function(){
+            var now, year, month, months;
+            now = new Date();
+            //var eid = new Date(2018, 12, 05);
 
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+            'November', 'December'];
+            
+            month = now.getMonth();
+
+            year  = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+            
+
+        },
 
         getDOMstrings: function(){
             return DOMstrings;
@@ -360,6 +375,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     return{
         init: function(){
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget:0,
                 totalInc:0,
